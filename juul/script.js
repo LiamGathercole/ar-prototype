f3815164ca3604c1ef8da43e1d6bd010769a6ca1
liamgathercole <liam.gathercole@gmail.com>
2019-07-31 16:01:01 +0100

    Update script.js

--- juul/script.js
+++ juul/script.js
@@ -3,10 +3,9 @@
 
 AFRAME.registerComponent('cloak', {
   init: function () {
-    var geometry = new THREE.BoxGeometry( 1.1, 1.0, 1.1 );
+    var geometry = new THREE.BoxGeometry( 2, 2, 2 );
     var material = new THREE.MeshBasicMaterial( {colorWrite: false} );
     var cube = new THREE.Mesh( geometry, material );
     this.el.object3D.add( cube );
   }
 });
-
